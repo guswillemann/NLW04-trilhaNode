5be7e61b84a4b605e49c31f7920fa6126b42d1ce
Gustavo Willemann <gustavowill@hotmail.com>
2021-02-24 16:54:59 -0300

    Projeto ao final da NLW04-Aula-02
    	new file:   ormconfig.json
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/controllers/UserController.ts
    	new file:   src/database/database.sqlite
    	new file:   src/database/index.ts
    	new file:   src/database/migrations/1614192178815-CreateUsers.ts
    	new file:   src/models/User.ts
    	new file:   src/routes.ts
    	modified:   src/server.ts
    	modified:   tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get('/', (request, reponse) => {
-  return reponse.json({ menssage: 'Hello World - NLW04' });
-});
-
-app.post('/', (request, reponse) => {
-  return reponse.json({ menssage: 'Os dados foram salvos com sucesso!' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running!'));
